834d3aff2d27affb94bfe7d39d4fc279e5108df7
Julien Chanseaume <julien.chanseaume@gmail.com>
2015-03-27 14:42:31 +0100

    Module configuration

---
+++ Model/Config/Base/CanadaPostConfigValue.php
@@ -0,0 +1,34 @@
+<?php
+/*************************************************************************************/
+/* This file is part of the Thelia package.                                          */
+/*                                                                                   */
+/* Copyright (c) OpenStudio                                                          */
+/* email : dev@thelia.net                                                            */
+/* web : http://www.thelia.net                                                       */
+/*                                                                                   */
+/* For the full copyright and license information, please view the LICENSE.txt       */
+/* file that was distributed with this source code.                                  */
+/*************************************************************************************/
+
+namespace CanadaPost\Model\Config\Base;
+
+/**
+ * Class CanadaPostConfigValue
+ * @package CanadaPost\Model\Config\Base
+ */
+class CanadaPostConfigValue
+{
+    const ENABLED = "enabled";
+    const MODE_PRODUCTION = "mode_production";
+    const CUSTOMER_NUMBER = "customer_number";
+    const USERNAME = "username";
+    const PASSWORD = "password";
+    const TEST_USERNAME = "test_username";
+    const TEST_PASSWORD = "test_password";
+    const QUOTE_TYPE_COMMERCIAL = "quote_type_commercial";
+    const CONTRACT_ID = "contract_id";
+    const INSURANCE = "insurance";
+    const ORIGIN_POSTALCODE = "origin_postalcode";
+    const DISALLOWED_SERVICES = "disallowed_services";
+}
+
---
+++ Model/Config/CanadaPostConfigValue.php
@@ -0,0 +1,24 @@
+<?php
+/*************************************************************************************/
+/* This file is part of the Thelia package.                                          */
+/*                                                                                   */
+/* Copyright (c) OpenStudio                                                          */
+/* email : dev@thelia.net                                                            */
+/* web : http://www.thelia.net                                                       */
+/*                                                                                   */
+/* For the full copyright and license information, please view the LICENSE.txt       */
+/* file that was distributed with this source code.                                  */
+/*************************************************************************************/
+
+namespace CanadaPost\Model\Config;
+
+use CanadaPost\Model\Config\Base\CanadaPostConfigValue as BaseCanadaPostConfigValue;
+
+/**
+ * Class CanadaPostConfigValue
+ * @package CanadaPost\Model\Config
+ */
+class CanadaPostConfigValue extends BaseCanadaPostConfigValue
+{
+}
+
